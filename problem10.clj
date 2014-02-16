94e54ff61c1e45862f574124a4d03362364215ca
NAKAJIMA Takaaki <takaaki.nakajima@mac.com>
2014-02-16 12:35:22 +0900

    solved problem 10

--- problem10.clj
+++ problem10.clj
@@ -1,33 +1,12 @@
 ;; Project Euler Problem 10
 ;; http://projecteuler.net/index.php?section=problems&id=10
 ;; Last modified by NAKAJIMA Takaaki, Sep 8, 2010
+;; Elapsed time: 40737.695 msecs (MBA 1.3GHz Intel Core i5)
 
 ; is the prime number?
-(defn prime?
-  ([n]
-   (every? false? (prime? n (- n 1))))
-  ([n x]
-   (lazy-seq
-    (if (> x 1)
-      (cons (zero? (rem n x)) (prime? n (dec x)))))))
-
-(defn prime-smart?
-  ([n]
-   (every? false? (prime-smart? n (- n 1))))
-  ([n x]
-   (lazy-seq
-    (if (> x 1)
-      (cons (zero? (rem n x)) (prime-smart? n (dec x)))))))
-
-; seq of the prime numbers
-(defn prime-seq
-  ([] (prime-seq 2))
-  ([n]
-   (lazy-seq
-    (if (true? (prime-smart? n))
-      (cons n (prime-seq (inc n)))
-      (prime-seq (inc n))))))
+(defn smart-prime? [n]
+    (every? #(pos? (rem n %)) (range 2 (Math/sqrt (inc n)))))
 
 ; Solve!
-
-(reverse (take 10 (prime-seq)))
+;(time (reduce + (filter smart-prime? (range 2 (inc 10)))))
+(time (reduce + (filter smart-prime? (range 2 (inc 2000000)))))
